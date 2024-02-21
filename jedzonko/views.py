762c393bf7a671e908986b58667c20dbb50c974d
P-fury <plutocamera@gmail.com>
2024-02-21 18:56:10 +0100

    2.1 Done

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -9,3 +9,9 @@ class IndexView(View):
     def get(self, request):
         ctx = {"actual_date": datetime.now()}
         return render(request, "index.html", ctx)
+
+
+class DashBoard(View):
+
+    def get(self, request):
+        return render(request, "dashboard.html")
