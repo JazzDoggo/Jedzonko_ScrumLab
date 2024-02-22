7ba0847fd65fc7aef147ac110e40dd4dd03eaa99
JazzDoggo <kacper.kozak.k@gmail.com>
2024-02-22 19:29:53 +0100

    5.1 Done

--- jedzonko/models.py
+++ jedzonko/models.py
@@ -11,3 +11,26 @@ class Recipe(models.Model):
     updated = models.DateTimeField(auto_now=True)
     preparation_time = models.TimeField()
     votes = models.IntegerField(default=0)
+
+
+class Plan(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    recipes = models.ManyToManyField(Recipe, through='RecipePlan')
+
+
+class DayName(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128)
+    order = models.IntegerField(unique=True)
+
+
+class RecipePlan(models.Model):
+    id = models.AutoField(primary_key=True)
+    meal_name = models.CharField(max_length=128)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    order = models.IntegerField(unique=True)
+    day_name = models.ForeignKey(DayName, on_delete=models.CASCADE)
