96fc2adfdfb2fe32034f519ec54634b1293d73f8
JazzDoggo <kacper.kozak.k@gmail.com>
2024-02-22 13:33:58 +0100

    Added 3 random recipes

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,13 +1,20 @@
 from datetime import datetime
+from random import shuffle
 
 from django.shortcuts import render
 from django.views import View
+from jedzonko.models import Recipe
 
 
 class IndexView(View):
 
     def get(self, request):
-        ctx = {"actual_date": datetime.now()}
+        recipes = list(Recipe.objects.all())
+        shuffle(recipes)
+        recipes = recipes[:3]
+
+        ctx = {"actual_date": datetime.now(),
+               "recipes": recipes}
         return render(request, "index.html", ctx)
 
 
