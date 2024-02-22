d53a161ddce080a29df1ec336ababc3a97a4b466
P-fury <plutocamera@gmail.com>
2024-02-22 15:21:25 +0100

    3.1 Done, New feature PAGINATOR, ?CONFLICT@URLS

--- jedzonko/views.py
+++ jedzonko/views.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+from django.core import paginator
+from django.core.paginator import Paginator
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -18,3 +21,16 @@ class DashBoard(View):
     def get(self, request):
         no_of_recipes = Recipe.objects.all().count()
         return render(request, "dashboard.html", {"recipes": no_of_recipes})
+
+
+class RecipesView(View):
+    def get(self, request):
+        recipes = Recipe.objects.all().order_by('-votes', '-created')
+        paginator = Paginator(recipes, 50)  ## LICZBA WYSWIETLANYCH KOMENTARZY NA STRONE
+        page_number = request.GET.get('page')
+        recipes_in_pages = paginator.get_page(page_number)
+        return render(request, "app-recipes.html", {"recipes": recipes_in_pages})
+
+class RecipeDetailView(View):
+    def get(self, request, id):
+        return HttpResponse('RECIPE DETAILS ' + id)
